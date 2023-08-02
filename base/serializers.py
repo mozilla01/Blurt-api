e93002cfeeed1b8a9645ec0e353c4866529c327a
mozilla01 <moizrocks12345@gmail.com>
2023-08-02 22:30:45 +0530

    feat: no. of comments on posts

--- base/serializers.py
+++ base/serializers.py
@@ -4,10 +4,14 @@ from .models import Post, Like, Reply
 
 class PostSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField("get_likes")
+    comments = serializers.SerializerMethodField("no_of_comments")
     image = serializers.ImageField(required=False)
 
     def get_likes(self, post):
         return Like.objects.filter(post=post.id).count()
+
+    def no_of_comments(self, post):
+        return Reply.objects.filter(post=post.id).count()
 
     class Meta:
         model = Post
